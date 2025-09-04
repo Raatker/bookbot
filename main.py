0545e9b1876dcdc41dfb305e3e48f1a928f8c314
Raatker <koziratomas@gmail.com>
2025-09-04 20:27:59 +1000

    K: Add a function on main.py and add .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetnigs boots")
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+main()
